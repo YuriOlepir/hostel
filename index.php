2e1836cd7e2bfc28f855956cda901af54adc85ec
ugear <olepir.yuri@gmail.com>
2016-09-07 11:36:32 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+include "header.php";
+?>
+<?php
+include "nav.php";
+?>
+
+        <div class="slider_container">
+            <div class="main_picture">
+                <img src="img_rooms/main.jpg" alt="pic">
+                <div class="flex-caption">Новый Хостел</div>
+            </div>
+
+            <?php
+            include "booking-form.php";
+            ?>
+        </div>
+
+        <section class="section_video hidden">
+            <div class="video_header">
+                <h2>Видео-экскурсия по хостелу</h2>
+            </div>
+            <div class="video_wrapper">
+                <div class="video_container">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/swmuqGWgZCc?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </section>
+    <section class="text_section">
+        <div class="container ">
+            <div class="row text_section_wrap">
+                <div class="col-md-4 col-sm-4 col_first">
+                    <h3>У нас Уютно
+                        <span class="title-end"></span></h3>
+
+                    У нас в гостях хорошо и уютно, тепло и радостно
+                </div>
+                <div class="col-md-4 col-sm-4 col_second">
+                    <h3>Хорошие условия
+                        <span class="title-end"></span></h3>
+
+                    Посетив наш хостел, вы будите приятно удивлены ценами.
+                    <div class="line_bottom">
+
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4 col_third">
+                    <h3>Наши удобства
+                        <span class="title-end"></span></h3>
+
+                    В каждой комнате есть сейф, чистые полотенца, и бесплатный Wi-Fi.
+                </div>
+            </div>
+        </div>
+
+
+    </section>
+
+    <section class="comments">
+        <h2>Отзывы наших гостей</h2>
+        <div class="container">
+            <div class="row comments_wrap">
+                <div class="col-md-6">
+                    <div class="comment_col">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam aut deleniti eius fugiat ipsa minus nemo perferendis reprehenderit repudiandae voluptate. Accusantium adipisci aperiam dolor dolores, dolorum eius iste modi! Eligendi?</lor></div>
+                    <div class="comment_col">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam blanditiis consequuntur iusto porro quas quia sit. Alias commodi deleniti eligendi excepturi, expedita impedit nam obcaecati, quasi repellendus sapiente, voluptatem!</div>
+                </div>
+                <div class="col-md-6">
+                    <div class="comment_col">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta doloribus error explicabo impedit, molestias nihil odit, omnis pariatur praesentium quaerat reprehenderit soluta velit. Deleniti inventore nobis optio possimus quia reprehenderit!</div>
+                    <div class="comment_col">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci aliquam aspernatur beatae commodi, culpa, distinctio ea, eaque earum eligendi esse harum illo incidunt odit perferendis quae sapiente vel voluptatibus.</div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+        <section class="section_maps">
+            <div class="maps_header">
+                <h2>Где мы находимся</h2></div>
+            <div class="maps_wrapper">
+                <div class="maps_container">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7903429.466027205!2d-90.73747802935453!3d14.707139096763143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f6a751a73b731cf%3A0x7ed1de82b6fb8264!2z0JPQvtC90LTRg9GA0LDRgQ!5e0!3m2!1sru!2sua!4v1459773920795" width="960" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </section>
+<?php
+include "footer.php";
+?>
